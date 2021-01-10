c0e8f751bbbfdbe98e38345b6cce697d53ccd5f3
MAHMOUD SALEM <mahmoud.salem1998@gmail.com>
2021-01-10 18:10:00 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/FlutterNewsApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.FlutterNewsApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: Colors.yellow,      
+    );
+  }
+}
